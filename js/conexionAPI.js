f824c374af65eddc0cddcc47a9bf342868ee2abf
SirZempo <uaemexzempo@gmail.com>
2024-06-08 00:00:40 -0600

    Se creo el fake api

---
+++ js/conexionAPI.js
@@ -0,0 +1,27 @@
+async function obtenerProductos() {
+    const conexion = await fetch("http://localhost:3001/prodcutos");
+    console.log(conexion);
+
+    const conexionConvertida = conexion.json();
+    console.log(conexionConvertida);
+
+    return conexionConvertida;
+}
+
+async function crearProducto(nombre, precio, url) {
+    const conexion = await fetch("http://localhost:3001/prodcutos", {
+        method: "POST",
+        headers: { "Content-type": "application/json" },
+        body: JSON.stringify({
+            nombre: nombre,
+            precio: precio,
+            url: url
+        })
+    });
+    const conexionConvertida = conexion.json();
+    return conexionConvertida;
+}
+
+export const conexionAPI = {
+    obtenerProductos
+};
